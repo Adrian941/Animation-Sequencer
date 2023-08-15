2adc6b8f912effc4b46fe8fab65063906c05e651
Pablo465 <adrianhuaxteco@gmail.com>
2023-08-15 17:15:32 -0600

    Revert "fix: metadata"
    This reverts commit 07b7ee81afcee90c4348513bb59eba2554f07e18.

--- Scripts/Runtime/Core/AnimationSequencerController.cs
+++ Scripts/Runtime/Core/AnimationSequencerController.cs
@@ -77,6 +77,9 @@ namespace BrunoMikoski.AnimationSequencer
         [SerializeField, Range(0, 1)] 
         private float progress = -1;
 
+        [SerializeField]
+        private GameObject rootGameObject;
+
         protected virtual void Awake()
         {
             progress = -1;
