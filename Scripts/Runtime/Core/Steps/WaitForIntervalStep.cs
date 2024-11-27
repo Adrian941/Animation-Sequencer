dbadc11c190b4592772de5659bbf5550eead5a1f
Pablo Huaxteco <adrianhuaxteco@gmail.com>
2024-11-27 13:51:06 -0600

    fix: Fixed "WaitForIntervalStep" duration calculation when used in Backwards" direction.
    Manual calculation is performed in "WaitForIntervalStep" due to a "sequence.Duration()" error when called in the "Backwards" direction, as it always returns zero.

--- Scripts/Runtime/Core/Steps/WaitForIntervalStep.cs
+++ Scripts/Runtime/Core/Steps/WaitForIntervalStep.cs
@@ -19,8 +19,17 @@ namespace BrunoMikoski.AnimationSequencer
             set => interval = Mathf.Clamp(value, 0, Mathf.Infinity);
         }
 
+        private float duration;
+
         public override Sequence GenerateTweenSequence()
         {
+            duration = Delay + interval;
+            if (duration == 0)
+            {
+                Debug.LogWarning($"The duration of the <b>\"{DisplayName}\"</b> Step is <b>\"Zero\"</b>. Please consider assigning a <b>\"Greater\"</b> value or removing the step.");
+                return null;
+            }
+
             Sequence sequence = DOTween.Sequence();
             sequence.SetDelay(Delay);
             sequence.AppendInterval(interval);
@@ -33,6 +42,12 @@ namespace BrunoMikoski.AnimationSequencer
         public override string GetDisplayNameForEditor(int index)
         {
             return $"{index}. Wait {Delay + interval} seconds";
+        }
+
+        public override float GetDuration()
+        {
+            //Manual calculation is performed here due to a "sequence.Duration()" error when called in the "Backwards" direction, as it always returns zero.
+            return sequence == null ? -1 : duration;
         }
     }
 }
